c0d7ff8c93c893d5d1b12ff3f34572fede3b1923
Gunnar Larsen <larsengu@oregonstate.edu>
2026-02-09 12:52:16 -0800

    navbar component

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const NavBar = () => {
+    return (
+        <nav>
+            <div>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/products">Products</NavLink>
+            <NavLink to="/vendors">Vendors</NavLink>
+            <NavLink to="/customers">Customers</NavLink>
+            <NavLink to="/warehouses">Warehouses</NavLink>
+            <NavLink to="/sales">Sales</NavLink>
+            <NavLink to="/purchases">Purchases</NavLink>
+            </div>
+        </nav>
+    );
+};
