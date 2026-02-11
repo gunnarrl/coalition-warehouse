a612314536625b96d72a8bcfb494755d143a5e67
Gunnar Larsen <larsengu@oregonstate.edu>
2026-02-10 17:05:25 -0800

    added DB reset button to homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
 const HomePage = () => {
+    const handleReset = () => {
+        if (window.confirm("Are you sure you want to reset the sample data?")) {
+            console.log("Resetting database...");
+        }
+    };
+
     return (
         <div>
-            <h1>Home Page</h1>
+            <h1>Coalition Warehouse Manager Home Page</h1>
+            <button onClick={handleReset}></button>
         </div>
     );
 }
