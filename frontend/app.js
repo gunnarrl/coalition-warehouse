c9851b88445ef54e0a044c514f3e5b93fcef4bcd
Gunnar Larsen <larsengu@oregonstate.edu>
2026-02-27 12:41:58 -0800

    Revert "fix?"
    This reverts commit b104834b57cfb5857f886f34a22d661e31ba9de6.

--- frontend/app.js
+++ frontend/app.js
@@ -1,16 +1,15 @@
 // Modified from app.js in activity 2
 const express = require('express');
-const path = require('path');
 const db = require('./db-connector');
 const app = express();
 const PORT = 30905;
 
-app.use(express.static(path.join(__dirname, 'dist')));
-
 app.get('/customers', async function (req, res) {
     try {
 
+        // Execute each query synchronously (await).
         const query = "SELECT * FROM Customers;"
+        // We want each query to finish before the next one starts.
         const [rows] = await db.query(query); // Store the results
 
         // Send the results to the browser
@@ -22,10 +21,6 @@ app.get('/customers', async function (req, res) {
         // Send a generic error message to the browser
         res.status(500).send("An error occurred while executing the database query.");
     }
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
